0acdbf8ffde497e15a91fd034f2b5c68879b92f6
Marta Krupinska <martadziedzic91@gmail.com>
2023-10-21 22:18:31 +0200

    dodanie podpowiedzi

--- js/actions.js
+++ js/actions.js
@@ -1,53 +1,106 @@
-function getBoardElement(row, col) {
-	/* let table = [];
-	const allRows = document.querySelectorAll('tr');
-	const allColumn = document.querySelectorAll('.board__dscrpt--col'); */
-	const allElements = Array.prototype.slice.call(
-		document.querySelectorAll('.board__square')
-	);
-
-	// const index =
-	const index = row * 8 + col;
-
-	//console.log(row, (row - 1) * 8, col - 1);
-	console.log(index, allElements[index]);
-
-	/* for (let i = 0; i < allRows.length - 1; i++) {
-		table[i] = [];
-		for (let j = 0; j < allColumn.length; j++) {
-			table[i][j] = j;
-		}
-	} */
-	return allElements[index];
-}
-//getBoardElement(2, 5);
-const discs = document.querySelectorAll('.disc');
-
-function showPossibleMoves() {
-	const activeDisc = document.querySelector('.disc:active');
-
-	if (!activeDisc) {
-		return;
+import { createHint } from './script.js';
+class Discs {
+	constructor(HTMLelement) {
+		this.HTMLelement = HTMLelement;
 	}
 
-	const row = activeDisc.closest('tr');
-	const col = Array.prototype.slice.call(row.querySelectorAll('td'));
+	getRowNumber() {
+		const row = this.HTMLelement.closest('tr');
+		const rowNumber = row.childNodes[1].textContent;
+		return parseInt(rowNumber);
+	}
+	getColNumber() {
+		const row = this.HTMLelement.closest('tr');
+		const col = Array.prototype.slice.call(row.querySelectorAll('td'));
 
-	const rowNumber = row.childNodes[1].textContent;
-	const colNumber = col.indexOf(activeDisc.parentElement);
+		return parseInt(col.indexOf(this.HTMLelement.parentElement));
+	}
+	getCountOfHints() {
+		/* czy dwie czy jedna ?? */
+	}
+}
 
-	//board__square
+class White extends Discs {
+	constructor(HTMLelement) {
+		super(HTMLelement);
+	}
+	findHintElements() {
+		const hintLeft = getBoardElement(
+			this.getRowNumber(),
+			this.getColNumber() - 2
+		);
+		const hintRight = getBoardElement(this.getRowNumber(), this.getColNumber());
 
-	console.log(rowNumber, colNumber);
+		return [hintLeft, hintRight].filter(function (hint) {
+			return hint !== undefined;
+		});
+	}
+}
+class Black extends Discs {
+	constructor(HTMLelement) {
+		super(HTMLelement);
+	}
+	findHintElements() {
+		const hintLeft = getBoardElement(
+			this.getRowNumber() - 2,
+			this.getColNumber() - 2
+		);
+		const hintRight = getBoardElement(
+			this.getRowNumber() - 2,
+			this.getColNumber()
+		);
 
-	const hintLeft = getBoardElement(
-		parseInt(rowNumber),
-		parseInt(colNumber) - 2
-	);
-	const hintRight = getBoardElement(parseInt(rowNumber), parseInt(colNumber));
-	console.log('hint1 ' + hintLeft);
+		return [hintLeft, hintRight].filter(function (hint) {
+			return hint !== undefined;
+		});
+	}
+}
+
+let hints;
+
+function getBoardElement(row, col) {
+	const allColumn = document.querySelectorAll('.board__square');
+	const index = row * 8 + col;
+
+	console.log(allColumn[index]);
+
+	if (allColumn[index].classList.contains('board__square--dark')) {
+		return allColumn[index];
+	}
+}
+
+const discs = document.querySelectorAll('.disc');
+
+function checkIfMoveIsPossible(disc) {}
+
+function showPossibleMoves() {
+	let disc;
+
+	const activeDisc = document.querySelector('.disc:active');
+	if (!activeDisc) {
+		return false;
+	}
+
+	if (activeDisc.classList.contains('disc--white')) {
+		disc = new White(activeDisc);
+	} else if (activeDisc.classList.contains('disc--black')) {
+		disc = new Black(activeDisc);
+	}
+
+	const hints = disc.findHintElements();
+	if (hints) {
+		createHint(hints);
+	}
+}
+
+function removePossibleMoves() {
+	const hints = document.querySelectorAll('.hint');
+	hints.forEach((hint) => {
+		hint.parentNode.removeChild(hint);
+	});
 }
 
 discs.forEach((disc) => {
-	disc.addEventListener('click', showPossibleMoves);
+	disc.addEventListener('mouseenter', showPossibleMoves, false);
+	disc.addEventListener('mouseleave', removePossibleMoves, false);
 });
--- js/script.js
+++ js/script.js
@@ -4,11 +4,44 @@ function createDisc(classColor) {
 	disc.classList.add('disc', 'disc--' + classColor);
 	return disc;
 }
+
 const setPiecesOnBoard = () => {
 	const boardPlaces = document.querySelectorAll('.board__square--dark');
 	const places = Array.prototype.slice.call(boardPlaces);
 
-	console.log(places[0].id);
+	for (let i = 0; i <= places.length - 1; i++) {
+		if (i < numberOfDiscs) {
+			boardPlaces[i].appendChild(createDisc('white'));
+		} else if (i >= places.length - numberOfDiscs) {
+			boardPlaces[i].appendChild(createDisc('black'));
+		}
+	}
+};
+function createHint(parents) {
+//	let hints = [];
+
+	parents.forEach((parent) => {
+		const hint = document.createElement('div');
+		hint.classList.add('hint');
+		parent.appendChild(hint);
+
+		//	hints.push(hint);
+	});
+
+	//	return hints;
+}
+
+setPiecesOnBoard();
+export { createHint };
+/* let numberOfDiscs = 12;
+function createDisc(classColor) {
+	const disc = document.createElement('div');
+	disc.classList.add('disc', 'disc--' + classColor);
+	return disc;
+}
+const setPiecesOnBoard = () => {
+	const boardPlaces = document.querySelectorAll('.board__square--dark');
+	const places = Array.prototype.slice.call(boardPlaces);
 
 	for (let i = 0; i <= places.length - 1; i++) {
 		if (i < numberOfDiscs) {
@@ -19,4 +52,13 @@ const setPiecesOnBoard = () => {
 	}
 };
 
+function createHint(parent) {
+	const hint = document.createElement('div');
+	hint.classList.add('hint');
+	parent.appendChild(hint);
+
+	return hint;
+}
 setPiecesOnBoard();
+
+export { createHint }; */
