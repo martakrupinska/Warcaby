8a7c36a75aa4e9216eb331871f4a7096fb6ea5b4
Marta Krupinska <martadziedzic91@gmail.com>
2023-10-25 20:48:41 +0200

    obsługa popup

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let numberOfDiscs = 12;
+let gamer;
 function createDisc(classColor) {
 	const disc = document.createElement('div');
 	disc.classList.add('disc', 'disc--' + classColor);
@@ -24,5 +25,53 @@ function createHint(parent) {
 }
 
 setPiecesOnBoard();
+
+const editBtns = document.querySelectorAll('.gamer__button--edit');
+
+function saveGamerName(e) {
+	const name = e.target.previousElementSibling.value;
+
+	if (!name) {
+		return;
+	}
+	const gamerNamePlace = gamer.parentElement.querySelector('.gamer__name');
+
+	gamerNamePlace.textContent = name;
+
+	const popup = e.target.parentElement;
+	popup.style.opacity = 0;
+}
+
+function cancelPopup(e) {
+	e.target.parentElement.style.opacity = 0;
+}
+
+function showPopUpWithUserName(e) {
+	gamer = e.target.parentElement.previousElementSibling;
+	const gamerName = gamer.textContent;
+
+	if (!gamer) {
+		return false;
+	}
+
+	const popup = document.querySelector('.popup');
+	const gamerNumber = popup.querySelector('.popup__dscrpt');
+	const gamerName1 = popup.querySelector('.popup__input');
+
+	gamerName1.value = '';
+
+	popup.style.opacity = 1;
+	gamerNumber.textContent = gamerName;
+
+	const saveBtn = popup.querySelector('.popup__button--accept');
+	const cancelBtn = popup.querySelector('.popup__button--cancel');
+
+	saveBtn.addEventListener('click', saveGamerName);
+	cancelBtn.addEventListener('click', cancelPopup);
+}
+
+editBtns.forEach((btn) => {
+	btn.addEventListener('click', showPopUpWithUserName);
+});
 
 export { createHint };
